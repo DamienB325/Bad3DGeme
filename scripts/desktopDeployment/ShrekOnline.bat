1c6b8ed1b968e7a8339093cc45c67036c5985b2f
DamienB325 <49534369+DamienB325@users.noreply.github.com>
2023-02-22 20:22:46 +0000

    BetterBread2

---
+++ scripts/desktopDeployment/ShrekOnline.bat
@@ -0,0 +1,2 @@
+"jre/bin/java" -XX:MaxRAMPercentage=60 -classpath "lib/*" org.amogustechnologies.ShrekOnline
+exit 0
---
+++ scripts/desktopDeployment/ShrekOnline.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+jre/bin/java -XX:MaxRAMPercentage=60 -classpath "lib/*" org.amogustechnologies.ShrekOnline
+exit 0
